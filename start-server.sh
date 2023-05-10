8f477c9d5d28ebb83461b94023773570a4db04ba
GE5YCPP <extern.michael.rudolph@porsche.de>
2023-05-10 14:30:43 +0200

    add #!/bin/bash to start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 _JAVA_XMX=1G
 _JAVA_XMS=1G
 
 if [[ -z "${JAVA_XMX}" ]]; then
-  JAVA_XMX=${_JAVA_XMX}
+  _JAVA_XMX=${JAVA_XMX}
 fi
 
 if [[ -z "${JAVA_XMS}" ]]; then
-  JAVA_XMS=${_JAVA_XMS}
+  _JAVA_XMS=${JAVA_XMS}
 fi
 
 java \
--Xms${_JAVA_XMS} \
--Xmx${_JAVA_XMX} \
+-Xms$_JAVA_XMS \
+-Xmx$_JAVA_XMX \
 -XX:+UseG1GC \
 -XX:+UnlockExperimentalVMOptions \
 -XX:MaxGCPauseMillis=100 \
