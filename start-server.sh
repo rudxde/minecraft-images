3846adb3b18e4f8a3d043672ed9158dd9f8a7488
GE5YCPP <extern.michael.rudolph@porsche.de>
2023-05-11 06:56:04 +0200

    fix start-server

--- start-server.sh
+++ start-server.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-_JAVA_XMX=1G
-_JAVA_XMS=1G
-
-if [[ -z "${JAVA_XMX}" ]]; then
-  _JAVA_XMX=${JAVA_XMX}
-fi
-
-if [[ -z "${JAVA_XMS}" ]]; then
-  _JAVA_XMS=${JAVA_XMS}
-fi
+JAVA_XMX=${JAVA_XMX:-1G}
+JAVA_XMS=${JAVA_XMS:-256M}
 
 java \
--Xms$_JAVA_XMS \
--Xmx$_JAVA_XMX \
+-Xms$JAVA_XMS \
+-Xmx$JAVA_XMX \
 -XX:+UseG1GC \
 -XX:+UnlockExperimentalVMOptions \
 -XX:MaxGCPauseMillis=100 \
